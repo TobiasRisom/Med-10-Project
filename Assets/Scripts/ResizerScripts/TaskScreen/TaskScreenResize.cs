89e27b8b94d7678aeff496ee9c4cd33d1ff62264
TobiasRisom <72734429+TobiasRisom@users.noreply.github.com>
2025-05-01 00:53:39 +0200

    Task Screen resized

---
+++ Assets/Scripts/ResizerScripts/TaskScreen/TaskScreenResize.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TaskScreenResize : MonoBehaviour
+{
+	public List<RectTransform> photoObject;
+	public List<RectTransform> allButtons;
+	public RectTransform texts;
+    void Start()
+    {
+	    int sh = Screen.height;
+
+	    if (sh < 2250)
+	    {
+		    foreach (RectTransform rt in photoObject)
+		    {
+			    rt.anchoredPosition = new Vector2(0, -675);
+		    }
+
+		    foreach (RectTransform rt in allButtons)
+		    {
+			    rt.anchoredPosition = new Vector2(0, 330);
+		    }
+
+		    texts.anchoredPosition = new Vector2(0, -552);
+		    texts.transform.GetChild(2)
+		         .GetComponent<TextMeshProUGUI>()
+		         .color = new Color(1, 1, 1, 0);
+	    }
+    }
+}
